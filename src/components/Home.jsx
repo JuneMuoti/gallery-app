3a376ce4fd3ff2a9a3c53a2619311bfd77838c54
JuneMuoti <june.muoti@gmail.com>
2023-01-27 23:30:53 +0300

    Add footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,183 @@
-import { Typography } from "@mui/material";
-
-
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { DataGrid } from '@mui/x-data-grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
+function Copyright() {
+    return (
+        
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Gallery App
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  // const columns = [
+  //   { field: 'id', headerName: 'ID', width: 70 },
+  //   { field: 'firstName', headerName: 'First name', width: 130 },
+  //   { field: 'lastName', headerName: 'Last name', width: 130 },
+  //   {
+  //     field: 'age',
+  //     headerName: 'Age',
+  //     type: 'number',
+  //     width: 90,
+  //   },
+  //   {
+  //     field: 'fullName',
+  //     headerName: 'Full name',
+  //     description: 'This column has a value getter and is not sortable.',
+  //     sortable: false,
+  //     width: 160,
+  //     valueGetter: (params) =>
+  //       `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  //   },
+  // ];
+
+  // const rows = [
+  //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  //   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  //   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  //   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  //   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  //   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  //   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // ];
 
 export default function Home() {
+  const [users,setUsers]=React.useState([]);
+  React.useEffect(()=>{
+    fetchData();
+  },[])
+  const fetchData = async () =>{
+    await fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>res.json())
+    .then((data)=>setUsers(data))
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  console.log(users)
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'name', headerName: 'Name', width: 300 },
+    { field: 'username', headerName: 'UserName', width: 300 },
+    { field: 'email', headerName: 'Email', width: 600 },
+    { field: 'phone', headerName: 'Phone', width: 300 },
+    { field: 'website', headerName: 'Website', width: 300 }
+  ]
+
   return (
-<div>
-    <Typography variant="h1">Home</Typography>
+    <main>
+        <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url("https://source.unsplash.com/random" )`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }} src="https://source.unsplash.com/random" alt="main image" />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            {/* <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              {post.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {post.description}
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              {post.linkText}
+            </Link> */}
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+   <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="xl">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+             USERS  
+            </Typography>
+         
+          </Container>
+        </Box>
+        <Container maxWidth="xl">
+    <div style={{ height: 600, width: '100%' }}>
+      <DataGrid
+        rows={users}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+
 </div>
+</Container>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+            
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Curative Gallery App to  showcase different work.
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+ 
+    </main>
   );
 }

0070bcbf3435b756a290bee3ce8f96eeada67ab9
JuneMuoti <june.muoti@gmail.com>
2023-01-28 09:25:35 +0300

    Rwemove footer details

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -18,19 +18,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useNavigate,useHistory} from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 // import ButtonBase from '@material-ui/core/ButtonBase';
+import Footer from './Footer';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-   Photosphere
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -80,20 +70,7 @@ navigateToPage = index => {
           >
             Albums for User {this.userId}
           </Typography>
-          {/* <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so folks
-            don&apos;t simply skip over it entirely.
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained">Main call to action</Button>
-            <Button variant="outlined">Secondary action</Button>
-          </Stack> */}
+        
         </Container>
       
       <Container sx={{ py: 8 }} maxWidth="md">
@@ -111,19 +88,7 @@ navigateToPage = index => {
  <Card
  sx={{ height: '100%', display: 'flex', flexDirection: 'column' ,bgcolor:'darkmagenta'}}
 >
- {/* <CardMedia
-   component="img"
-   sx={{
-     // 16:9
-     pt: '56.25%',
-   }}
-   image="https://source.unsplash.com/random"
-   alt="random"
- /> */}
- {/* <Navigate to="/Photo"  onClick={this.navigateToPage.bind(
-     this,
-     album.id,
- )}/> */}
+
  <CardContent sx={{ flexGrow: 1 }}
  key={album.id}
  onClick={this.navigateToPage.bind(
@@ -153,21 +118,7 @@ navigateToPage = index => {
       </Container>
     </main>
     {/* Footer */}
-    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-      <Typography variant="h6" align="center" gutterBottom>
-        Footer
-      </Typography>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="text.secondary"
-        component="p"
-      >
-         Curative Gallery App to  showcase different work.
-      </Typography>
-      <Copyright />
-    </Box>
-    {/* End footer */}
+   
   </ThemeProvider>
 
   );
@@ -204,98 +155,3 @@ getAlbums() {
 export default Album;
 
 
-// export default function Album() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-    
-//       <main>
-//         {/* Hero unit */}
-//         <Box
-//           sx={{
-//             bgcolor: 'background.paper',
-//             pt: 8,
-//             pb: 6,
-//           }}
-//         >
-//           <Container maxWidth="sm">
-//             <Typography
-//               component="h1"
-//               variant="h2"
-//               align="center"
-//               color="text.primary"
-//               gutterBottom
-//             >
-//               Album layout
-//             </Typography>
-//             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-//               Something short and leading about the collection below—its contents,
-//               the creator, etc. Make it short and sweet, but not too short so folks
-//               don&apos;t simply skip over it entirely.
-//             </Typography>
-//             <Stack
-//               sx={{ pt: 4 }}
-//               direction="row"
-//               spacing={2}
-//               justifyContent="center"
-//             >
-//               <Button variant="contained">Main call to action</Button>
-//               <Button variant="outlined">Secondary action</Button>
-//             </Stack>
-//           </Container>
-//         </Box>
-//         <Container sx={{ py: 8 }} maxWidth="md">
-//           {/* End hero unit */}
-//           <Grid container spacing={4}>
-//             {cards.map((card) => (
-//               <Grid item key={card} xs={12} sm={6} md={4}>
-//                 <Card
-//                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-//                 >
-//                   <CardMedia
-//                     component="img"
-//                     sx={{
-//                       // 16:9
-//                       pt: '56.25%',
-//                     }}
-//                     image="https://source.unsplash.com/random"
-//                     alt="random"
-//                   />
-//                   <CardContent sx={{ flexGrow: 1 }}>
-//                     <Typography gutterBottom variant="h5" component="h2">
-//                       Heading
-//                     </Typography>
-//                     <Typography>
-//                       This is a media card. You can use this section to describe the
-//                       content.
-//                     </Typography>
-//                   </CardContent>
-//                   <CardActions>
-//                     <Button size="small">View</Button>
-//                     <Button size="small">Edit</Button>
-//                   </CardActions>
-//                 </Card>
-//               </Grid>
-//             ))}
-//           </Grid>
-//         </Container>
-//       </main>
-//       {/* Footer */}
-//       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-//         <Typography variant="h6" align="center" gutterBottom>
-//           Footer
-//         </Typography>
-//         <Typography
-//           variant="subtitle1"
-//           align="center"
-//           color="text.secondary"
-//           component="p"
-//         >
-//            Curative Gallery App to  showcase different work.
-//         </Typography>
-//         <Copyright />
-//       </Box>
-//       {/* End footer */}
-//     </ThemeProvider>
-//   );
-// }
